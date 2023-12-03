9786a992099db75ff224465e8c7130d228da8a03
Steven Op de beeck <steven.opdebeeck@kunlabora.be>
2023-12-03 21:53:44 +0100

    part2: replace all values, doesn't work on input

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -2,11 +2,31 @@ package day01
 
 import readInput
 
+val digitStrings: List<String> = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
 fun main() {
-    val sum = readInput("day01", "input")
-        .map {
-            it.first { c -> c.isDigit() }.digitToInt() * 10
-            +it.last { c -> c.isDigit() }.digitToInt()
-        }.sum()
-    print("Day01: $sum")
+    val inputStrings = readInput("day01", "input")
+    println("Day01/1: ${parseCalibrationValues(inputStrings)}")
+    println("Day01/2: ${parseCalibrationValues(inputStrings, ::digitStringMapper)}")
 }
+
+fun digitStringMapper(value: String): String {
+    var newValue = value
+    do {
+        val firstHit = digitStrings
+            .mapIndexed { index, str -> index to newValue.indexOf(str) }
+            .filter { it.second != -1 }
+            .minByOrNull { it.second }
+            ?.let { newValue.replaceFirst(digitStrings[it.first], (it.first + 1).toString()) }
+        firstHit?.let { newValue = firstHit }
+    } while (firstHit != null)
+    return newValue
+}
+
+fun parseCalibrationValues(inputStrings: List<String>, valueMapper: (String) -> String = { it }) =
+    inputStrings
+        .map(valueMapper)
+        .sumOf { str ->
+            str.first { it.isDigit() }.digitToInt() * 10 +
+                    str.last { it.isDigit() }.digitToInt()
+        }
---
+++ src/test/kotlin/day01/Day01KtTest.kt
@@ -0,0 +1,34 @@
+package day01
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+
+class Day201KtTest : FunSpec({
+    test("part 1 calibration fivethreeonezblqnsfk1") {
+        parseCalibrationValues(listOf("fivethreeonezblqnsfk1")) shouldBe 11
+    }
+    test("part 1 calibration treb7uchet") {
+        parseCalibrationValues(listOf("treb7uchet")) shouldBe 77
+    }
+    test("part 1 calibration 1abc2") {
+        parseCalibrationValues(listOf("1abc2")) shouldBe 12
+    }
+    test("part 2 calibration fivethreeonezblqnsfk1") {
+        parseCalibrationValues(listOf("fivethreeonezblqnsfk1"), ::digitStringMapper) shouldBe 51
+    }
+    test("part 2 calibration two1nine") {
+        parseCalibrationValues(listOf("two1nine"), ::digitStringMapper) shouldBe 29
+    }
+    test("part 2 example input") {
+        val values = """two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        """.trimIndent().lines()
+        parseCalibrationValues(values, ::digitStringMapper) shouldBe 281
+    }
+})
